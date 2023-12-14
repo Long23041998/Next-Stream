7cfdf50e18917fedd6d360ad50bf279a07526b14
Long23041998 <Tkcualong1998@z3fl2.onmicrosoft.com>
2023-12-14 17:00:37 +0700

    add logout

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
 import Image from 'next/image'
+import { UserButton } from "@clerk/nextjs";
 
 
 
 export default function Home() {
   return (
-    <p>only authed user can see this</p>
+    <div>
+
+      <p>only authed user can see this</p>
+      <UserButton afterSignOutUrl="/" />
+
+
+    </div>
 
   )
 }
